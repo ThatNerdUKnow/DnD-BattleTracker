699ba39b76ae7e9e6824d61d82f007c9a1675333
withenex <brandonpi265@gmail.com>
2021-05-28 20:42:01 -0500

    Calculate xp multiplier

--- src/components/encounter/encounter.vue
+++ src/components/encounter/encounter.vue
@@ -1,68 +1,67 @@
 <template>
-    <div class="">
-        <h2>Encounter</h2>{{totalXP}}
-        <div id="encounter" class="container-fluid overflow-auto">
-        
-        <div class="row overflow-auto">
-        <div class='col-lg-6' v-for="monster in monsters" :key="monster.n">
-            <monster :monster="monster" @remove="remove($event)"></monster>
-        </div>
-        </div>
-        </div>
+  <div class="">
+    <h2>Encounter</h2>
+    <div>
+      <strong>XP: {{ totalXP }} X Multiplier ({{ multiplier }}) = ({{totalXP*multiplier}})</strong>
     </div>
+    <div id="encounter" class="container-fluid overflow-auto">
+      <div class="row overflow-auto">
+        <div class="col-lg-6" v-for="monster in monsters" :key="monster.n">
+          <monster :monster="monster" @remove="remove($event)"></monster>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import monster from '../monster.vue'
-const tools = require('5eutils')
-import {xpChart} from '../../utils/utils.js'
+import monster from "../monster.vue";
+const tools = require("5eutils");
+import { xpChart, xpMultiplier } from "../../utils/utils.js";
 
 export default {
-    name: "encounter",
-    components: {
-        monster
+  name: "encounter",
+  components: {
+    monster,
+  },
+  props: {
+    monsters: Array,
+  },
+  methods: {
+    remove(data) {
+      this.$emit("remove", data);
     },
-    props:{
-        monsters: Array,
+  },
+  computed: {
+    combatRating: function () {
+      var acc = 0;
+      this.monsters.forEach((monster) => {
+        acc = acc + tools.Parser.crToNumber(monster.cr);
+      });
+      return acc;
     },
-    methods:{
-        remove(data)
-        {
-            
-            this.$emit("remove",data)
+    totalXP: function () {
+      var acc = 0;
+      this.monsters.forEach((monster) => {
+        if (monster.cr) {
+          var monsterXP = xpChart[monster.cr];
+
+          acc = acc + monsterXP;
         }
+      });
+      return acc;
     },
-    computed:{
-        combatRating: function(){
-            var acc = 0;
-            this.monsters.forEach(monster=>{
-                
-                acc = acc+ tools.Parser.crToNumber(monster.cr)
-            })
-            return acc;
-        },
-        totalXP: function()
-        {
-            var acc = 0;
-            this.monsters.forEach(monster=>{
-                
-                if(monster.cr)
-                {
-                var monsterXP = xpChart[monster.cr]
-                
-                acc = acc + monsterXP
-                }
-                
-            })
-            return acc;
-        }
-    }
-}
+    multiplier: function () {
+      var num = this.monsters.length;
+      console.log(num);
+      const multiplier = xpMultiplier(num);
+      console.log(multiplier);
+      return multiplier;
+    },
+  },
+};
 </script>
 <style scoped>
-#encounter
-{
-    height: 90vh;
-    
-    
+#encounter {
+  height: 90vh;
 }
 </style>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,52 +1,74 @@
 
-    const attackTypes = {
-        m: "Melee Attack",
-        r: "Ranged Attack",
-        "m,r": "Melee or Ranged Attack",
-        mw: "Melee Weapon Attack",
-        rw: "Ranged Weapon Attack",
-        ms: "Melee Spell Attack",
-        rs: "Ranged Spell Attack",
-        "mw,rw": "Melee/Ranged Weapon"
+const attackTypes = {
+    m: "Melee Attack",
+    r: "Ranged Attack",
+    "m,r": "Melee or Ranged Attack",
+    mw: "Melee Weapon Attack",
+    rw: "Ranged Weapon Attack",
+    ms: "Melee Spell Attack",
+    rs: "Ranged Spell Attack",
+    "mw,rw": "Melee/Ranged Weapon"
+}
+
+
+
+const xpChart = {
+    "0": 10,
+    "1/8": 25,
+    "1/4": 50,
+    "1/2": 100,
+    "1": 200,
+    "2": 450,
+    "3": 700,
+    "4": 1100,
+    "5": 1800,
+    "6": 2300,
+    "7": 2900,
+    "8": 3900,
+    "9": 5000,
+    "10": 5900,
+    "11": 7200,
+    "12": 8400,
+    "13": 10000,
+    "14": 11500,
+    "15": 13000,
+    "16": 13000,
+    "17": 15000,
+    "18": 18000,
+    "19": 20000,
+    "20": 25000,
+    "21": 33000,
+    "22": 41000,
+    "23": 50000,
+    "24": 62000,
+    "25": 75000,
+    "26": 90000,
+    "27": 105000,
+    "28": 120000,
+    "29": 135000,
+    "30": 155000,
+
+}
+
+function xpMultiplier(enemies) {
+    const multipliers = {
+        "1": 1,
+        "2": 1.5,
+        "3-6": 2,
+        "7-10": 2.5,
+        "11-14": 3,
+        "15+": 4
     }
 
-    
-
-    const xpChart = {
-        "0": 10,
-        "1/8": 25,
-        "1/4": 50,
-        "1/2": 100,
-        "1": 200,
-        "2": 450,
-        "3": 700,
-        "4": 1100,
-        "5": 1800,
-        "6": 2300,
-        "7": 2900,
-        "8": 3900,
-        "9": 5000,
-        "10": 5900,
-        "11": 7200,
-        "12": 8400,
-        "13": 10000,
-        "14": 11500,
-        "15": 13000,
-        "16": 13000,
-        "17": 15000,
-        "18": 18000,
-        "19": 20000,
-        "20": 25000,
-        "21": 33000,
-        "22": 41000,
-        "23": 50000,
-        "24": 62000,
-        "25": 75000,
-        "26": 90000,
-        "27": 105000,
-        "28": 120000,
-        "29": 135000,
-        "30": 155000,
-        
+    if (enemies == 1) {
+        return multipliers["1"]
     }
-  module.exports = {attackTypes,xpChart}
+    else if (enemies == 2) { return multipliers["2"] }
+    else if (enemies >= 3 && enemies <= 6) { return multipliers["3-6"] }
+    else if (enemies >= 7 && enemies <= 10) { return multipliers["7-10"] }
+    else if (enemies >= 11 && enemies <= 14) { return multipliers["11-14"] }
+    else if (enemies >= 15) { return multipliers["15+"] }
+
+
+}
+module.exports = { attackTypes, xpChart, xpMultiplier }
